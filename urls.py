207d5b36a3591ee6dfc402454574d3a9dbe46876
Jaroslav Keznikl <greg@kezniklnb.(none)>
2011-03-14 12:30:06 +0100

    missing files, minor  fixes

--- urls.py
+++ urls.py
@@ -1,14 +1,13 @@
-
 from django.conf.urls.defaults import *
 from django.contrib import admin
 
 from mezzanine.core.views import direct_to_template
-
+from mezzanine.conf import settings
 
 admin.autodiscover()
 
-# Add the urlpatterns for any custom Django applications here. 
-# You can also change the ``home`` view to add your own functionality to 
+# Add the urlpatterns for any custom Django applications here.
+# You can also change the ``home`` view to add your own functionality to
 # the project's homepage.
 urlpatterns = patterns("",
     ("^admin/", include(admin.site.urls)),
@@ -16,3 +15,8 @@ urlpatterns = patterns("",
     ("^news/", include("news.urls")),
     ("^", include("mezzanine.urls")),
 )
+
+if settings.DEBUG:
+    urlpatterns = patterns('',
+        (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    ) + urlpatterns
